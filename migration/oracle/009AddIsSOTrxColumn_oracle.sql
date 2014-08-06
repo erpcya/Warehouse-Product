81506f2b7aac6e1153baee6af032be0cc1f7468e
Yamel Senih <Yamel Senih@localhost>
2014-08-06 12:03:14 +0200

    Add Validation Is Sales Order Transaction

---
+++ migration/oracle/009AddIsSOTrxColumn_oracle.sql
@@ -0,0 +1,35 @@
+-- 6/08/2014 11:44:02 AM CEST
+-- Warehouse-Product
+INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Table_ID,ColumnName,Created,CreatedBy,Description,EntityType,FieldLength,Help,IsActive,IsAllowLogging,IsAlwaysUpdateable,IsAutocomplete,IsEncrypted,IsIdentifier,IsKey,IsMandatory,IsParent,IsSelectionColumn,IsSyncDatabase,IsTranslated,IsUpdateable,Name,SeqNo,Updated,UpdatedBy,Version) VALUES (0,74154,1106,0,20,53847,'IsSOTrx',TO_DATE('2014-08-06 11:44:00','YYYY-MM-DD HH24:MI:SS'),100,'This is a Sales Transaction','ECA02',1,'The Sales Transaction checkbox indicates if this item is a Sales Transaction.','Y','Y','N','N','N','N','N','N','N','N','N','N','N','Sales Transaction',0,TO_DATE('2014-08-06 11:44:00','YYYY-MM-DD HH24:MI:SS'),100,0)
+;
+
+-- 6/08/2014 11:44:02 AM CEST
+-- Warehouse-Product
+INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Column_ID=74154 AND NOT EXISTS (SELECT * FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
+;
+
+-- 6/08/2014 11:44:30 AM CEST
+-- Warehouse-Product
+INSERT INTO AD_Field (AD_Client_ID,AD_Column_ID,AD_Field_ID,AD_Org_ID,AD_Tab_ID,Created,CreatedBy,Description,DisplayLength,EntityType,Help,HideInListView,IsActive,IsCentrallyMaintained,IsDisplayed,IsEncrypted,IsFieldOnly,IsHeading,IsReadOnly,IsSameLine,Name,PreferredWidth,SeqNo,SortNo,Updated,UpdatedBy) VALUES (0,74154,74287,0,53987,TO_DATE('2014-08-06 11:44:28','YYYY-MM-DD HH24:MI:SS'),100,'This is a Sales Transaction',0,'ECA02','The Sales Transaction checkbox indicates if this item is a Sales Transaction.','N','Y','Y','Y','N','N','N','N','N','Sales Transaction',0,110,0,TO_DATE('2014-08-06 11:44:28','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 6/08/2014 11:44:30 AM CEST
+-- Warehouse-Product
+INSERT INTO AD_Field_Trl (AD_Language,AD_Field_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Field_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Field t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Field_ID=74287 AND NOT EXISTS (SELECT * FROM AD_Field_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Field_ID=t.AD_Field_ID)
+;
+
+-- 6/08/2014 11:45:06 AM CEST
+-- Warehouse-Product
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=100,Updated=TO_DATE('2014-08-06 11:45:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=74287
+;
+
+-- 6/08/2014 11:45:06 AM CEST
+-- Warehouse-Product
+UPDATE AD_Field SET IsDisplayed='Y', SeqNo=110,Updated=TO_DATE('2014-08-06 11:45:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=74146
+;
+
+-- 6/08/2014 11:45:24 AM CEST
+-- Warehouse-Product
+UPDATE AD_Field SET IsSameLine='Y',Updated=TO_DATE('2014-08-06 11:45:24','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=74146
+;
+
