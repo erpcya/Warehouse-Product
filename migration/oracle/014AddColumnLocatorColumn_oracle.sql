6fefb9b46baed89c6510f05fb43e3b2535d66fb6
waditza <waditza>
2014-08-19 16:05:24 -0530

    Add Column Locator

---
+++ migration/oracle/014AddColumnLocatorColumn_oracle.sql
@@ -0,0 +1,25 @@
+-- 19/08/2014 02:56:12 PM VET
+-- Warehouse Product
+INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Reference_Value_ID,ColumnName,Created,CreatedBy,EntityType,FieldLength,IsActive,Name,PrintName,Updated,UpdatedBy) VALUES (0,57424,0,18,3,'Locator_Column_ID',TO_DATE('2014-08-19 14:56:07','YYYY-MM-DD HH24:MI:SS'),100,'ECA02',10,'Y','Locator Column','Locator Column',TO_DATE('2014-08-19 14:56:07','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- 19/08/2014 02:56:12 PM VET
+-- Warehouse Product
+INSERT INTO AD_Element_Trl (AD_Language,AD_Element_ID, Description,Help,Name,PO_Description,PO_Help,PO_Name,PO_PrintName,PrintName, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Element_ID, t.Description,t.Help,t.Name,t.PO_Description,t.PO_Help,t.PO_Name,t.PO_PrintName,t.PrintName, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Element t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Element_ID=57424 AND NOT EXISTS (SELECT * FROM AD_Element_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Element_ID=t.AD_Element_ID)
+;
+
+-- 19/08/2014 02:56:44 PM VET
+-- Warehouse Product
+UPDATE AD_Element_Trl SET Name='Columna Localizador',PrintName='Columna Localizador',Updated=TO_DATE('2014-08-19 14:56:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=57424 AND AD_Language='es_MX'
+;
+
+-- 19/08/2014 03:00:57 PM VET
+-- Warehouse Product
+INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Reference_ID,AD_Reference_Value_ID,AD_Table_ID,AD_Val_Rule_ID,ColumnName,Created,CreatedBy,EntityType,FieldLength,IsActive,IsAllowLogging,IsAlwaysUpdateable,IsAutocomplete,IsEncrypted,IsIdentifier,IsKey,IsMandatory,IsParent,IsSelectionColumn,IsSyncDatabase,IsTranslated,IsUpdateable,Name,SeqNo,Updated,UpdatedBy,Version) VALUES (0,74318,57424,0,18,3,53847,100,'Locator_Column_ID',TO_DATE('2014-08-19 15:00:56','YYYY-MM-DD HH24:MI:SS'),100,'ECA02',10,'Y','Y','N','N','N','N','N','N','N','N','N','N','Y','Locator Column',0,TO_DATE('2014-08-19 15:00:56','YYYY-MM-DD HH24:MI:SS'),100,0)
+;
+
+-- 19/08/2014 03:00:57 PM VET
+-- Warehouse Product
+INSERT INTO AD_Column_Trl (AD_Language,AD_Column_ID, Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Column_ID, t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Column t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Column_ID=74318 AND NOT EXISTS (SELECT * FROM AD_Column_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Column_ID=t.AD_Column_ID)
+;
+
